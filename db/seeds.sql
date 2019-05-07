83a68d51a172b69dee4ec0c3adf30ca3d2d15bbd
Michael Putterflam <michael.putterflam@gmail.com>
2019-05-06 23:17:46 -0700

    created basic server and db schema

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ("Pesto Cheese Burger");
+INSERT INTO burgers (burger_name) VALUES ("Western Bacon Burger");
+INSERT INTO burgers (burger_name) VALUES ("Double Double Burger");
